c5c902becf4bbbcea7f02dab44e9f9f3c696e1bb
Vlad Paulechka <vlad9.ru@gmail.com>
2023-10-04 11:08:19 +0300

    добавление фильтров для текстур

--- src/images/textures.js
+++ src/images/textures.js
@@ -1,4 +1,4 @@
-import {TextureLoader} from "three";
+import {NearestFilter, RepeatWrapping, TextureLoader} from "three";
 import {dirtImg, grassImg, glassImg, woodImg, logImg} from './images'
 
 const dirTexture = new TextureLoader().load(dirtImg)
@@ -6,6 +6,16 @@ const grassTexture = new TextureLoader().load(grassImg)
 const glassTexture = new TextureLoader().load(glassImg)
 const woodTexture = new TextureLoader().load(woodImg)
 const logTexture = new TextureLoader().load(logImg)
+
+woodTexture.magFilter = NearestFilter;
+dirTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+
+grassTexture.magFilter = NearestFilter
+grassTexture.wrapS = RepeatWrapping
+grassTexture.wrapT = RepeatWrapping
+
 
 
 export {
